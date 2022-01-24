8e4c277d69b195fbdedf0af8fe091c168781f86e
Yu-Chi Ho <45426941+Brianiiix@users.noreply.github.com>
2022-01-24 20:24:09 +0800

    Add files via upload

---
+++ HW5/kernel2.cu
@@ -0,0 +1,60 @@
+#include <cuda.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+__device__ int mandel(float c_re, float c_im, int count)
+{
+    float z_re = c_re, z_im = c_im;
+    int i;
+    for (i = 0; i < count; ++i)
+    {
+        if (z_re * z_re + z_im * z_im > 4.f)
+            break;
+
+        float new_re = z_re * z_re - z_im * z_im;
+        float new_im = 2.f * z_re * z_im;
+        z_re = c_re + new_re;
+        z_im = c_im + new_im;
+    }
+    return i;
+}
+
+__global__ void mandelKernel(float lowerX, float lowerY, float stepX, float stepY, int *d_img, int resX, int resY, int maxIterations) {
+    // To avoid error caused by the floating number, use the following pseudo code
+    //
+    // float x = lowerX + thisX * stepX;
+    // float y = lowerY + thisY * stepY;
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    int j = blockDim.y * blockIdx.y + threadIdx.y;
+
+    float x = lowerX + i * stepX;
+    float y = lowerY + j * stepY;
+
+    int index = j * resX + i;
+    d_img[index] = mandel(x, y, maxIterations);
+}
+
+// Host front-end function that allocates the memory and launches the GPU kernel
+void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, int resX, int resY, int maxIterations)
+{
+    float stepX = (upperX - lowerX) / resX;
+    float stepY = (upperY - lowerY) / resY;
+
+    int* d_img;
+    size_t size = resX * resY * sizeof(int);
+    size_t pitch;
+	cudaMallocPitch(&d_img, &pitch, resX * sizeof(int), resY);
+
+    dim3 threadsPerBlock(16, 16);
+    dim3 numBlocks(resX / threadsPerBlock.x, resY / threadsPerBlock.y);
+    mandelKernel<<<numBlocks, threadsPerBlock>>>(lowerX, lowerY, stepX, stepY, d_img, resX, resY, maxIterations);
+    cudaDeviceSynchronize();
+
+    int* h_img;
+    cudaHostAlloc(&h_img, size, cudaHostAllocMapped);
+    cudaMemcpy(h_img, d_img, size, cudaMemcpyDeviceToHost);
+	memcpy(img, h_img, size);
+
+    cudaFree(d_img);
+    cudaFreeHost(h_img);
+}
---
+++ HW5/kernel3.cu
@@ -0,0 +1,66 @@
+#include <cuda.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+__device__ int mandel(float c_re, float c_im, int count)
+{
+    float z_re = c_re, z_im = c_im;
+    int i;
+    for (i = 0; i < count; ++i)
+    {
+        if (z_re * z_re + z_im * z_im > 4.f)
+            break;
+
+        float new_re = z_re * z_re - z_im * z_im;
+        float new_im = 2.f * z_re * z_im;
+        z_re = c_re + new_re;
+        z_im = c_im + new_im;
+    }
+    return i;
+}
+
+__global__ void mandelKernel(float lowerX, float lowerY, float stepX, float stepY, int row, int col, int *d_img, int resX, int resY, int maxIterations) {
+    // To avoid error caused by the floating number, use the following pseudo code
+    //
+    // float x = lowerX + thisX * stepX;
+    // float y = lowerY + thisY * stepY;
+    int i = (blockDim.x * blockIdx.x + threadIdx.x) * row;
+    int j = (blockDim.y * blockIdx.y + threadIdx.y) * col;
+
+    for(int b = j; b < j + col && b < resY; b++){
+        for(int a = i; a < i + row && a < resX; a++){
+            float x = lowerX + a * stepX;
+            float y = lowerY + b * stepY;
+
+            int index = b * resX + a;
+            d_img[index] = mandel(x, y, maxIterations);
+        }
+    }
+}
+
+// Host front-end function that allocates the memory and launches the GPU kernel
+void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, int resX, int resY, int maxIterations)
+{
+    float stepX = (upperX - lowerX) / resX;
+    float stepY = (upperY - lowerY) / resY;
+
+    int* d_img;
+    size_t size = resX * resY * sizeof(int);
+    size_t pitch;
+	cudaMallocPitch(&d_img, &pitch, resX * sizeof(int), resY);
+
+    int row = 2;
+    int col = 2;
+    dim3 threadsPerBlock(16 / row, 16 / col);
+    dim3 numBlocks(resX / threadsPerBlock.x, resY / threadsPerBlock.y);
+    mandelKernel<<<numBlocks, threadsPerBlock>>>(lowerX, lowerY, stepX, stepY, row, col, d_img, resX, resY, maxIterations);
+    cudaDeviceSynchronize();
+
+    int* h_img;
+    cudaHostAlloc(&h_img, size, cudaHostAllocMapped);
+    cudaMemcpy(h_img, d_img, size, cudaMemcpyDeviceToHost);
+	memcpy(img, h_img, size);
+
+    cudaFree(d_img);
+    cudaFreeHost(h_img);
+}
